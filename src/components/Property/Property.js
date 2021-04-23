eae7d0f515c02d036ca306cae067cb731c4abca4
lkmSasanga <lkmsasanga@gmail.com>
2021-04-24 00:42:54 +0530

    working on receiving data

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import classes from './Property.module.css';
 import Card from "../UI/Card/Card";
 import galle from '../Media/galle.jpg';
 
 const Property = (props) => {
+    const [incomingData, setIncomingData] = useState('');
 
-    console.log('FROM PROPERTY',props.hotelData[0].hotels.h1.name);
-    console.log('FROM PROPERTY',props.hotelData[0]);
+    useEffect(() => {
+        // console.log('FROM PROPERTY',props.hotelData[0].hotels.h1.name);
+        console.log('FROM PROPERTY',props.hotelData.hotels);
+        setIncomingData(props.hotelData)
+
+    },[]);
+    // const listItems = incomingData.map((d) => <li>{d}}</li>);
+
     return (
         <div>
             <h1 className={classes.heading}>Around Galle</h1>
             <div>
                 <img className={classes.image} src={galle} alt="Galle Image" />
+                {incomingData &&
+                <ul>
+                    {/*{incomingData.map(d => <li key={d.hotels.name}>{d}}</li>)}*/}
+                </ul>}
+
+
             </div>
 
             <Card>
