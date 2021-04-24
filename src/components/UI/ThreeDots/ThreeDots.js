6e9765ea88dd6c7a4bf4a512626a15996f9d310e
lkmSasanga <lkmsasanga@gmail.com>
2021-04-24 21:14:32 +0530

    threedots ui element added

---
+++ src/components/UI/ThreeDots/ThreeDots.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './ThreeDots.module.css';
+const threeDots = () => (
+    <div className={classes.loader}>Loading...</div>
+)
+// color 00C7FD
+
+export default threeDots;
