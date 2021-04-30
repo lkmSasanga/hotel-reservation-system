e383d8331cf3799507e84f6ac30b72cd8eca32ac
lkmSasanga <lkmsasanga@gmail.com>
2021-05-01 00:18:45 +0530

    showign hotels success in user routes

--- src/components/Customer/GetHotels.js
+++ src/components/Customer/GetHotels.js
@@ -4,36 +4,46 @@ import classes from './GetHotels.module.css';
 import CHeader from "./CHeader/CHeader";
 import Spinner from "../UI/Spinner/Spinner";
 import Card from "../UI/Card/Card";
+import {useHistory} from "react-router-dom";
 
 const GetHotels = () => {
     const [loggedUserToken, setLoggedUserToken] = useState('');
     const [hotelDetails, setHotelDetails] = useState('');
     const [showSpinner, setShowSpinner] = useState(false);
+    const [clickedTown, setClickedTown] = useState();
 
+
+    const history = useHistory();
 
     useEffect(() => {
         setLoggedUserToken(localStorage.getItem('token'));
+        setClickedTown(localStorage.getItem('town'));
         setShowSpinner(true);
 
-        fetch('http://localhost:5000/api/get_hotels', {
-            method: 'GET',
-            headers: {
-                'Authorization': `${localStorage.getItem('token')}`,
-                // 'Content-Type': 'application/json',
-            },
-        }).then(res => res.json())
-            .then(json => {
-                if (json.success) {
-                    console.log('success');
-                    console.log(typeof(json.data));
-                    console.log(json.data);
+        if (1) {
+            fetch(`http://localhost:5000/api/get_hotels?city=${encodeURIComponent(localStorage.getItem('town'))}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `${localStorage.getItem('token')}`,
+                    // 'Content-Type': 'application/json',
+                },
+            }).then(res => res.json())
+                .then(json => {
+                    if (json.success) {
+                        console.log('success');
+                        console.log(typeof(json.data));
+                        console.log(json.data);
+                        console.log(localStorage.getItem('town'))
+                        console.log(clickedTown)
 
-                    setShowSpinner(false);
-                    setHotelDetails(json.data)
-                } else {
-                    console.log('Error Occurred');
-                }
-            })
+                        setShowSpinner(false);
+                        setHotelDetails(json.data)
+                    } else {
+                        console.log('Error Occurred');
+                    }
+                })
+        }
+
 
     },[]);
 
@@ -48,13 +58,16 @@ const GetHotels = () => {
             <p className={classes.subHeading}>These popular destinations have a lot to offer</p>
             {hotelDetails &&
             <div className={classes.row}>
-
-                {hotelDetails.map((town) =>
-                    <div key={town._id} >
+                {hotelDetails.map((hotel) =>
+                    <div key={hotel._id} >
                         <Card classname={classes.cardBody}>
                             <div className={classes.oneHotel}>
-                                {/*<img alt="" className={classes.image} src={town.image}/>*/}
-                                {/*<p className={classes.header}>{town.name}</p>*/}
+                                <img alt="" className={classes.image} src={hotel.image}/>
+                                <p className={classes.header}>{hotel.hotel_name}</p>
+                                <p className={classes.header}>Location: {hotel.city}</p>
+                                <p className={classes.header}>Rate: {hotel.rate}</p>
+                                <p className={classes.header}>Rooms Available: {hotel.rooms_available}</p>
+                                <p className={classes.header}>Price: {hotel.price}</p>
 
                             </div>
                         </Card>
