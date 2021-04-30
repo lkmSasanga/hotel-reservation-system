0eebf7e5ea2dfe6d0ee630e40d358a1a2692a719
lkmSasanga <lkmsasanga@gmail.com>
2021-04-30 19:21:25 +0530

    town names showed using list

--- src/components/Customer/GetTowns.js
+++ src/components/Customer/GetTowns.js
@@ -2,13 +2,17 @@ import React, {useEffect, useState} from 'react';
 
 import classes from './GetTowns.module.css';
 import CHeader from "./CHeader/CHeader";
+import Spinner from "../UI/Spinner/Spinner";
 
 const GetTowns = () => {
     const [loggedUserToken, setLoggedUserToken] = useState('');
     const [townsDetails, setTownsDetails] = useState('');
+    const [showSpinner, setShowSpinner] = useState(false);
+
 
     useEffect(() => {
         setLoggedUserToken(localStorage.getItem('token'));
+        setShowSpinner(true);
 
         fetch('http://localhost:5000/api/get_towns', {
             method: 'GET',
@@ -18,10 +22,15 @@ const GetTowns = () => {
             },
         }).then(res => res.json())
             .then(json => {
-                console.log(json.data)
-                setTownsDetails(json.data)
                 if (json.success) {
                     console.log('success');
+                    console.log(typeof(json.data));
+                    console.log(json.data);
+
+                    setShowSpinner(false);
+                    setTownsDetails(json.data)
+                } else {
+                    console.log('Error Occurred');
                 }
             })
 
@@ -38,14 +47,24 @@ const GetTowns = () => {
         // })
     },[]);
 
+    const townsList = () => {
+        return townsDetails.map((town) => <li key={town._id}>{town.name}</li>);
+    };
 
     return (
       <div className={classes.main}>
           <CHeader/>
           <h1 className={classes.heading}>Towns</h1>
           <p className={classes.subHeading}>These popular destinations have a lot to offer</p>
+          {townsDetails &&
+          <div>
+              {townsDetails.map((town) => <li key={town._id}>{town.name}</li>)}
+          </div>
+          }
+
+          {showSpinner && <Spinner/>}
       </div>
     );
-};
+}
 
 export default GetTowns;
--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import classes from './Spinner.module.css';
-const spinner = () => (
+const Spinner = () => (
     <div className={classes.loader}>Loading...</div>
 )
 // color 00C7FD
 
-export default spinner;
+export default Spinner;
