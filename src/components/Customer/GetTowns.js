97f8413699c92104798f70cef5901e191af416eb
lkmSasanga <lkmsasanga@gmail.com>
2021-04-30 18:10:25 +0530

    towns recieved from the api

--- src/components/Customer/GetTowns.js
+++ src/components/Customer/GetTowns.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import classes from './GetTowns.module.css';
 import CHeader from "./CHeader/CHeader";
 
 const GetTowns = () => {
+    const [loggedUserToken, setLoggedUserToken] = useState('');
+
+    useEffect(() => {
+        setLoggedUserToken(localStorage.getItem('token'));
+
+        fetch('http://localhost:5000/api/get_towns', {
+            method: 'GET',
+            headers: {
+                'Authorization': `${localStorage.getItem('token')}`,
+                // 'Content-Type': 'application/json',
+            },
+        }).then(res => res.json())
+            .then(json => {
+                console.log(json)
+                if (json.success) {
+                    console.log('success');
+                }
+            })
+
+        // axios.get('http://localhost:5000/api/get_towns', {
+        //     method: 'GET',
+        //     headers: {
+        //         'Authorization': `${loggedUserToken}`,
+        //     }
+        // }).then(response => {
+        //     let output = response.data
+        //     console.log(output);
+        // }).catch((error) => {
+        //     console.log(error);
+        // })
+    },[]);
+
+
     return (
-      <div>
+      <div className={classes.main}>
           <CHeader/>
-          <h1>Get Towns</h1>
+          <h1 className={classes.heading}>Towns</h1>
+          <p className={classes.subHeading}>These popular destinations have a lot to offer</p>
       </div>
     );
 };
