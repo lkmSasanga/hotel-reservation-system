8db26b6e9ea5fb287060c5786e24ce9c1da1c584
lkmSasanga <lkmsasanga@gmail.com>
2021-05-03 20:37:35 +0530

    try to fix memory leak problem in user gettowns route

--- src/components/Customer/GetTowns.js
+++ src/components/Customer/GetTowns.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import classes from './GetTowns.module.css';
 import CHeader from "./CHeader/CHeader";
@@ -8,55 +8,39 @@ import {useHistory} from "react-router-dom";
 import Background from '../../assets/bg2.png';
 
 const GetTowns = () => {
-    const [loggedUserToken, setLoggedUserToken] = useState('');
+    // const [loggedUserToken, setLoggedUserToken] = useState('');
     const [townsDetails, setTownsDetails] = useState('');
     const [showSpinner, setShowSpinner] = useState(false);
 
     const history = useHistory();
 
+    let isRendered = useRef(false);
     useEffect(() => {
-        setLoggedUserToken(localStorage.getItem('token'));
+        // let isMounted = true;
         setShowSpinner(true);
 
         fetch('http://localhost:5000/api/get_towns', {
             method: 'GET',
             headers: {
                 'Authorization': `${localStorage.getItem('token')}`,
-                // 'Content-Type': 'application/json',
             },
         }).then(res => res.json())
             .then(json => {
                 if (json.success) {
-                    // console.log('success');
-                    // console.log(typeof(json.data));
-                    // console.log(json.data);
-
                     setShowSpinner(false);
                     setTownsDetails(json.data)
                 } else {
                     console.log('Error Occurred');
                 }
             })
-
-        // axios.get('http://localhost:5000/api/get_towns', {
-        //     method: 'GET',
-        //     headers: {
-        //         'Authorization': `${loggedUserToken}`,
-        //     }
-        // }).then(response => {
-        //     let output = response.data
-        //     console.log(output);
-        // }).catch((error) => {
-        //     console.log(error);
-        // })
+        // return () => { isMounted = false };
+        return () => {
+            isRendered = false;
+        };
     },[]);
 
-    // const townsList = () => {
-    //     return townsDetails.map((town) => <li key={town._id}>{town.name}</li>);
-    // };
 
     const gotoHotels = (town) => {
-        // e.preventDefault();
         history.push("/get_hotels");
         localStorage.setItem('town', town);
         console.log(town);
@@ -72,16 +56,12 @@ const GetTowns = () => {
         backgroundRepeat: 'no-repeat'
     };
 
-
     return (
       <div className={classes.main}>
           <section style={ sectionStyle }>
               <CHeader/>
-
               <h1 className={classes.heading}>Explore Holiday Resorts</h1>
               <p className={classes.subHeading}>These popular destinations have a lot to offer</p>
-
-
 
           {townsDetails &&
           <div className={classes.row}>
@@ -92,16 +72,13 @@ const GetTowns = () => {
                           <div className={classes.oneTown} >
                               <img alt="" className={classes.image} src={town.image}/>
                               <p className={classes.header} >{town.name}</p>
-
                           </div>
                       </Card>
-
 
                   </div>
                   )}
           </div>
           }
-
           {showSpinner && <Spinner/>}
           </section>
       </div>
