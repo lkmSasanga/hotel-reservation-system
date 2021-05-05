c355afdc89feff0f4a102f3dd35342a0d7b4626e
lkmSasanga <lkmsasanga@gmail.com>
2021-05-05 11:22:15 +0530

    404 page edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return(
         <div className={classes.body}>
             <h1 className={classes.header}>404</h1>
-            <p className={classes.paragraph}>Oops! Something is wrong.</p>
+            <p className={classes.paragraph}>Oops! This page is not Available.</p>
             <a className={classes.button} href="#" ><i className={classes.iconHome}></i> Go back in initial page, is better.</a>
             <p className={classes.spaceControl}>.</p>
         </div>
